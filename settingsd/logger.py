6c1c5923bfd1b4d3459227168b2133ca2c7c486d
Devaev Maxim <mdevaev@etersoft.ru>
2010-09-06 22:56:44 +0400

    Added some types of messaging, optimized trace decorator

--- settingsd/logger.py
+++ settingsd/logger.py
@@ -8,13 +8,41 @@ import config
 
 
 #####
-def message(log_level, message) :
-	text_log_levels_list = [
-		"%s [ Info ]:" % (const.MY_NAME), # const.LOG_LEVEL_INFO == 0
-		"%s [ Details ]:" % (const.MY_NAME), # const.LOG_LEVEL_VERBOSE == 1
-		"%s [ Debug ]:" % (const.MY_NAME) # # const.LOG_LEVEL_DEBUG == 2
-	]
+ERROR_MESSAGE = 0
+INFO_MESSAGE = 1
+VERBOSE_MESSAGE = 2
+DEBUG_MESSAGE = 3
+
+ALL_MESSAGES_LIST = (
+	ERROR_MESSAGE,
+	INFO_MESSAGE,
+	VERBOSE_MESSAGE,
+	DEBUG_MESSAGE
+)
+
+
+#####
+class UnknownMessageType(Exception) :
+	pass
+
+
+#####
+def message(message_type, message) :
+	if message_type in (ERROR_MESSAGE, INFO_MESSAGE) :
+		log_level = const.LOG_LEVEL_INFO
+	elif message_type == VERBOSE_MESSAGE :
+		log_level = const.LOG_LEVEL_VERBOSE
+	elif message_type == DEBUG_MESSAGE :
+		log_level = const.LOG_LEVEL_DEBUG
+	else :
+		raise UnknownMessageType("Message type \"%d\" not in list %s" % (message_type, ALL_MESSAGES_LIST))
 
 	if log_level <= config.value("service", "log_level") :
+		text_log_levels_list = (
+			"%s [ Info ]:" % (const.MY_NAME),
+			"%s [ Details ]:" % (const.MY_NAME), 
+			"%s [ Debug ]:" % (const.MY_NAME)
+		)
+
 		print >> sys.stderr, text_log_levels_list[log_level], message
 

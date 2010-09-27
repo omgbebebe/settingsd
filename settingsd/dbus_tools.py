6c448ca02594c170f3ede129f9a48aa35f8a646c
Devaev Maxim <mdevaev@etersoft.ru>
2010-09-27 17:25:06 +0400

    Remove own join() implementation

--- settingsd/dbus_tools.py
+++ settingsd/dbus_tools.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
-##### Private methods #####
-def joinItems(separator, first, *others_list) :
-	for others_list_item in others_list :
-		if len(first) == 0 :
-			first = others_list_item
-		else :
-			first += separator + others_list_item
-	return first
-
-
 ##### Public methods #####
 def joinPath(first, *others_list) :
-	return joinItems("/", first, *others_list)
+	return "/".join((first,) + others_list)
 
 def joinMethod(first, *others_list) :
-	return joinItems(".", first, *others_list)
+	return ".".join((first,) + others_list)
 

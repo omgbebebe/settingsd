6d8494ada33b13dcb4a8e2826e778571101eb42a
Devaev Maxim <mdevaev@etersoft.ru>
2011-01-29 18:35:24 +0300

    Рефакторинг

--- settingsd/validators/os.py
+++ settingsd/validators/os.py
@@ -8,12 +8,12 @@ from common import ValidatorError
 
 ##### Public methods #####
 def validUserName(arg) :
-	if re.match("^[a-z_][a-z0-9_-]*$", arg) == None :
+	if re.match(r"^[a-z_][a-z0-9_-]*$", arg) == None :
 		raise ValidatorError("Argument \"%s\" is not valid UNIX user name" % (arg))
 	return arg
 
 def validGroupName(arg) :
-	if re.match("^[a-z_][a-z0-9_-]*$", arg) == None :
+	if re.match(r"^[a-z_][a-z0-9_-]*$", arg) == None :
 		raise ValidatorError("Argument \"%s\" is not valid UNIX group name" % (arg))
 	return arg
 
